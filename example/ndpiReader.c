ac2795100fb32cc84dac7251c9db5feee25a97ac
ydy1234 <yyddyylince@126.com>
2018-05-15 09:54:42 +0800

    #6 add all protocol for PBR
    Add all protocol for PBR choose

--- example/ndpiReader.c
+++ example/ndpiReader.c
@@ -60,13 +60,32 @@
 
 #include "ndpi_util.h"
 
-int saveProtoCnt=32;
-char* saveProto[]={"QQ","Yahoo","SSH","MySQL","Hotmail","VMware",
-	               "BitTorrent","AVI","Flash","MPEG","QuickTime",
-	                "RTSP","PPStream","QQLive","Thunder","Telnet",
-	                "SCTP","OSPF","EGP","Facebook","Twitter","YouTube",
-	                "NetFlow","OpenVPN","Oracle","Amazon","eBay","CNN",
-	                "QuickPlay","Sina","LISP","AmazonVideo"};
+int saveProtoCnt=243;
+char* saveProto[]={"Unknown","FTP_CONTROL","POP3","SMTP","IMAP","DNS","IPP","HTTP","MDNS","NTP",
+"NetBIOS","NFS","SSDP","BGP","SNMP","XDMCP","SMB","Syslog","DHCP","PostgreSQL",
+"MySQL","Hotmail","Direct_Download_Link","POPS","AppleJuice","DirectConnect","ntop","COAP","VMware","SMTPS",
+"Filetopia","UBNTAC2","Kontiki","OpenFT","FastTrack","Gnutella","eDonkey","BitTorrent","EPP","AVI",
+"Flash","OggVorbis","MPEG","QuickTime","RealMedia","WindowsMedia","MMS","Xbox","QQ","PlaceholderA",
+"RTSP","IMAPS","IceCast","PPLive","PPStream","Zattoo","ShoutCast","Sopcast","Tvants","TVUplayer",
+"HTTP_Download","QQLive","Thunder","Soulseek","SSL_No_Cert","IRC","Ayiya","Unencrypted_Jabber","MSN","Oscar",
+"Yahoo","BattleField","GooglePlus","VRRP","Steam","HalfLife2","WorldOfWarcraft","Telnet","STUN","IPsec",
+"GRE","ICMP","IGMP","EGP","SCTP","OSPF","IP_in_IP","RTP","RDP","VNC",
+"PcAnywhere","SSL","SSH","Usenet","MGCP","IAX","TFTP","AFP","Stealthnet","Aimini",
+"SIP","TruPhone","ICMPV6","DHCPV6","Armagetron","Crossfire","Dofus","Fiesta","Florensia","Guildwars",
+"HTTP_Application_ActiveSync","Kerberos","LDAP","MapleStory","MsSQL-TDS","PPTP","Warcraft3","WorldOfKungFu","Slack","Facebook",
+"Twitter","Dropbox","GMail","GoogleMaps","YouTube","Skype","Google","DCE_RPC","NetFlow","sFlow",
+"HTTP_Connect","HTTP_Proxy","Citrix","NetFlix","LastFM","Waze","YouTubeUpload","ICQ","CHECKMK","AJP",
+"Apple","Webex","WhatsApp","AppleiCloud","Viber","AppleiTunes","Radius","WindowsUpdate","TeamViewer","Tuenti",
+"LotusNotes","SAP","GTP","UPnP","LLMNR","RemoteScan","Spotify","WebM","H323","OpenVPN",
+"NOE","CiscoVPN","TeamSpeak","Tor","CiscoSkinny","RTCP","RSYNC","Oracle","Corba","UbuntuONE",
+"Whois-DAS","Collectd","SOCKS","Nintendo","RTMP","FTP_DATA","Wikipedia","ZeroMQ","Amazon","eBay",
+"CNN","Megaco","Redis","Pando_Media_Booster","VHUA","Telegram","Vevo","Pandora","QUIC","WhatsAppVoice",
+"EAQ","Ookla","AMQP","KakaoTalk","KakaoTalk_Voice","Twitch","QuickPlay","WeChat","MPEG_TS","Snapchat",
+"Sina(Weibo)","GoogleHangout","IFLIX","Github","BJNP","1kxun","iQIYI","SMPP","DNScrypt","TINC",
+"Deezer","Instagram","Microsoft","Starcraft","Teredo","HotspotShield","HEP","GoogleDrive","OCS","Office365",
+"Cloudflare","MS_OneDrive","MQTT","RX","AppleStore","OpenDNS","Git","DRDA","PlayStore","SOMEIP",
+"FIX","Playstation","Pastebin","LinkedIn","SoundCloud","CSGO","LISP","Diameter","ApplePush","GoogleServices",
+"AmazonVideo","GoogleDocs","WhatsAppFile"};
 /** Client parameters **/
 static char *_pcap_file[MAX_NUM_READER_THREADS]; /**< Ingress pcap file/interfaces */
 static FILE *playlist_fp[MAX_NUM_READER_THREADS] = { NULL }; /**< Ingress playlist */
